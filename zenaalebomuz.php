c0fe2be5957f145e017c2d3c6c7e2149362dfbc5
martin.skarbala <martin.skarbala@itera.no>
2017-07-27 15:09:55 +0200

    updated conchita

--- zenaalebomuz.php
+++ zenaalebomuz.php
@@ -8,15 +8,27 @@
         font-family: Permanent Marker, sans-serif;
         font-size: 50px;
     }
-    img{
-        max-width:800px;
-        -webkit-box-shadow: 0px 0px 179px -17px rgba(0,0,0,0.75);
-        -moz-box-shadow: 0px 0px 179px -17px rgba(0,0,0,0.75);
-        box-shadow: 0px 0px 179px -17px rgba(0,0,0,0.75);
+
+    img {
+        margin-top: 50px;
+        max-width: 800px;
+        -webkit-box-shadow: 0px 0px 179px -19px rgba(0, 0, 0, 0.45);
+        -moz-box-shadow: 0px 0px 179px -19px rgba(0, 0, 0, 0.45);
+        box-shadow: 0px 0px 179px -19px rgba(0, 0, 0, 0.45);
     }
 
-    div.radio input{
-        float:left;
+    label {
+        font-size: 30px;
+        margin: 20px;
+
+    }
+
+    input[type=radio] {
+        margin: 10px;
+    }
+
+    h1.description {
+        font-size: 60px;
     }
 
 </style>
@@ -25,17 +37,24 @@
 <?php include 'partials/navigation.php' ?>
 <div class="container">
     <div class="align-middle text-center">
-        <h1>Zena alebo muz?</h1>
+        <h1>
+            <span style="color:darkred">Zena</span> alebo <span style="color: darkblue">muz</span>?</h1>
         <img src="img/conchita.jpg" alt="">
-        <div class="radio">
-            <label><input type="radio" name="optradio">Muz</label>
-        </div>
-        <div class="radio">
-            <label><input type="radio" name="optradio">Zena</label>
+        <div>
+            <label><input type="radio" value="wurst" name="optradio">Muz</label>
+            <label><input type="radio" value="conchita" name="optradio">Zena</label>
         </div>
     </div>
+    <h1 class="description text-center"></h1>
 </div>
 </body>
-
+<script>
+    $(document).ready(function () {
+        $("input").change(function () {
+            var text = $(this).val();
+            $("h1.description").text("It's " + text);
+        })
+    })
+</script>
 
 </html>
