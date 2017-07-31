7a32c290cd528c73bdfe7869bf4d1feac2c22806
msk1 <skarbala.martin@gmail.com>
2017-07-31 19:47:41 +0200

    brano update

--- zjavenie.php
+++ zjavenie.php
@@ -8,27 +8,40 @@
         font-family: Permanent Marker, sans-serif;
         font-size: 50px;
     }
+    #hideHim{
+        display: none;
+    }
+    img{
+        margin: 20px auto;
+        display: block;
+        width: auto;
+    }
+    button.btn{
+        border-radius: 0px;
+    }
 </style>
 <body>
 <?php include 'partials/navigation.php' ?>
     <div class="container">
         <h1 class ="text-center">Zjavenie</h1>
-        <div class="row">
-            <button id="showHim" class="btn  btn-lg">ukaz mi</button>
-            <button id="hideHim" class= "btn btn-lg">prec s diablom</button>
-            <div class="row">
+        <div class="row col-md-8 col-md-offset-2">
+            <button id="showHim" class="btn btn-default btn-lg btn-block">ukaz mi</button>
                 <img style="display: none" src="img/brano.jpg" alt="brano" class="brano">
-            </div>
+            <button id="hideHim" class= "btn btn-lg btn-danger btn-block">prec s diablom !!!</button>
         </div>
     </div>
 </body>
 <script>
 $(document).ready(function() {
     $("#showHim").click(function() {
-        $(".brano").delay(1500).fadeIn(1500);
+        $("#hideHim").delay(1500).show(0);
+        $(".brano").delay(1000).fadeIn(500);
+        $(this).hide();
     });
      $("#hideHim").click(function() {
         $(".brano").fadeOut(500);
+        $(this).hide();
+         $("#showHim").delay(500).show(0);
     })
 });
 
