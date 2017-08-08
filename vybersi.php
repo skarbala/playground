9a8609cdf4f5771a5f22ae0dfc9c15b8474a5540
martin.skarbala <martin.skarbala@itera.no>
2017-08-08 14:40:56 +0200

    added background image design

--- vybersi.php
+++ vybersi.php
@@ -4,22 +4,39 @@
 <?php include 'partials/head.php'; ?>
 <link href="https://fonts.googleapis.com/css?family=Roboto:500i" rel="stylesheet">
 <style>
-    h1 {
-        font-family: Roboto, sans-serif;
-        font-size: 50px;
+    body {
+        background: url("img/pokemon/background.jpg") no-repeat center center fixed;
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
     }
-    div.pokemon{
+    h1{
+        margin-bottom: 30px;
+    }
+
+    h1,h3 {
+        font-family: Roboto, sans-serif;
+        font-size: 70px;
+        color:#FFFFFF;
+        text-shadow: 3px 3px 2px #68696b;
+    }
+
+
+    div.pokemon {
         margin-top: 50px;
     }
-    select{
+
+    select {
         margin-top: 30px;
     }
+
 </style>
 <body>
 <?php include 'partials/navigation.php' ?>
 <div class="container">
     <h1 class="text-center">Select 'em all!</h1>
-    <div class="col-md-4 col-md-offset-4">
+    <div class="col-md-6 col-md-offset-3">
         <select class="form-control">
             <option value="">select your pokemon</option>
             <option value="01">Charmander</option>
@@ -29,8 +46,8 @@
             <option value="05">Diglett</option>
             <option value="06">Geodude</option>
         </select>
-        <div class="pokemon text-center">
-        </div>
+    </div>
+    <div class="col-md-10 col-md-offset-1 pokemon text-center">
     </div>
 </div>
 </body>
@@ -42,7 +59,7 @@
             if (selectedPokemon.val()) {
                 $("div.pokemon")
                     .html("<img src=\"img/pokemon/" + selectedPokemon.val() + ".png\" alt=\"\">")
-                    .append("<h3>I choose you "+selectedPokemon.text()+" !</h3>");
+                    .append("<h3>I choose you " + selectedPokemon.text() + " !</h3>");
             }
         });
 
