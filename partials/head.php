798adce7e137890bd37858088531ba5978998390
msk1 <skarbala.martin@gmail.com>
2017-08-28 19:39:24 +0200

    updated title of page

--- partials/head.php
+++ partials/head.php
@@ -1,8 +1,13 @@
+<?php
+$current_page = basename($_SERVER['REQUEST_URI'], ".php");
+
+?>
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Ihrisko</title>
+    <title>Ihrisko | <?php echo ucfirst($current_page)?> </title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
     <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
 </head>
+
