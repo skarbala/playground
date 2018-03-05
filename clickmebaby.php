ff54aecc193dbc983862b2f1e1d849a07b16e2d8
martin.skarbala <martin.skarbala@itera.no>
2018-03-05 10:40:15 +0100

    reworked existing JS using VUE.js

--- clickmebaby.php
+++ clickmebaby.php
@@ -1,86 +1,83 @@
 <!DOCTYPE html>
-<html>
+<html xmlns:v-on="http://www.w3.org/1999/xhtml">
+<script src="https://cdn.jsdelivr.net/npm/vue"></script>
 <link href="https://fonts.googleapis.com/css?family=Lobster+Two" rel="stylesheet"><?php include 'partials/head.php'; ?>
 <style>
-    h1 {
-        font-family: Lobster two, Arial, serif;
-        font-size: 50px;
-    }
+  h1 {
+    font-family: Lobster two, Arial, serif;
+    font-size: 50px;
+  }
 
-    body {
-        background-image: url("img/pink.png");
-        color: whitesmoke;
-    }
+  body {
+    background-image: url("img/pink.png");
+    color: whitesmoke;
+  }
 
-    button {
-        font-size: 24px;
-        width: 150px;
-        height: 150px;
-        text-align: center;
-        border: solid 5px whitesmoke;
-        border-radius: 50%;
-        user-focus: none;
-        margin: 100px auto 80px auto;
+  button {
+    font-size: 24px;
+    width: 150px;
+    height: 150px;
+    text-align: center;
+    border: solid 5px whitesmoke;
+    border-radius: 50%;
+    user-focus: none;
+    margin: 100px auto 80px auto;
 
-        display: block;
-        background-color: transparent;
-        font-weight: 600;
-        -webkit-box-shadow: 0 8px 0 #c5376d, 0 15px 20px rgba(0, 0, 0, .35);
-        -moz-box-shadow: 0 8px 0 #c5376d, 0 15px 20px rgba(0, 0, 0, .35);
-        box-shadow: 0 8px 0 #c5376d, 0 15px 20px rgba(0, 0, 0, .35);
-        -webkit-transition: -webkit-box-shadow .1s ease-in-out;
-        -moz-transition: -moz-box-shadow .1s ease-in-out;
-        -o-transition: -o-box-shadow .1s ease-in-out;
-        transition: box-shadow .1s ease-in-out;
+    display: block;
+    background-color: transparent;
+    font-weight: 600;
+    -webkit-box-shadow: 0 8px 0 #c5376d, 0 15px 20px rgba(0, 0, 0, .35);
+    -moz-box-shadow: 0 8px 0 #c5376d, 0 15px 20px rgba(0, 0, 0, .35);
+    box-shadow: 0 8px 0 #c5376d, 0 15px 20px rgba(0, 0, 0, .35);
+    -webkit-transition: -webkit-box-shadow .1s ease-in-out;
+    -moz-transition: -moz-box-shadow .1s ease-in-out;
+    -o-transition: -o-box-shadow .1s ease-in-out;
+    transition: box-shadow .1s ease-in-out;
 
-    }
+  }
 
-    button:focus {
-        outline: none;
-    }
+  button:focus {
+    outline: none;
+  }
 
-    button:active {
-        -webkit-box-shadow: 0 0 0 #c5376d, 0 0 0 rgba(0, 0, 0, .3);
-        -moz-box-shadow: 0 0 0 #c5376d, 0 0 0 rgba(0, 0, 0, .3);
-        box-shadow: 0 0 0 #c5376d, 0 0 0 rgba(0, 0, 0, .3);
-        -webkit-transform: translate(0, 4px);
-        -moz-transform: translate(0, 4px);
-        -o-transform: translate(0, 4px);
-        transform: translate(0, 4px);
-    }
+  button:active {
+    -webkit-box-shadow: 0 0 0 #c5376d, 0 0 0 rgba(0, 0, 0, .3);
+    -moz-box-shadow: 0 0 0 #c5376d, 0 0 0 rgba(0, 0, 0, .3);
+    box-shadow: 0 0 0 #c5376d, 0 0 0 rgba(0, 0, 0, .3);
+    -webkit-transform: translate(0, 4px);
+    -moz-transform: translate(0, 4px);
+    -o-transform: translate(0, 4px);
+    transform: translate(0, 4px);
+  }
 
 </style>
 <body>
 <?php include 'partials/navigation.php' ?>
 
 <div class="container">
-    <h1 class="text-center">Click me baby one more time</h1>
-    <div class="row">
-        <button id="clickMe">Click me !</button>
+  <h1 class="text-center">Click me baby one more time</h1>
+  <div class="row">
+  </div>
+  <button v-on:click="counter += 1">Click me !</button>
+  <div class="row text-center">
+    <h1 style="font-size: 60px" id="clicks">{{counter}}</h1>
+    <div class="description">
+      <p v-if="counter==1">klik</p>
+      <p v-else-if="counter>=2 && counter<=4">kliky</p>
+      <p v-else>klikov</p>
     </div>
-    <div class="row text-center">
-        <h1 style="font-size: 60px" id="clicks">0</h1>
-        <p class="description">klikov</p>
-    </div>
+  </div>
 </div>
 </body>
 
 <script>
-    var clicks = 0;
-    $(document).ready(function () {
-        $("#clickMe").click(function () {
-            $("#clicks").text(++clicks);
-            if (clicks == 1) {
-                $(".description").text("klik");
-            }
-            if (clicks == 2 || clicks == 3 || clicks == 4) {
-                $(".description").text("kliky");
-            }
-            if (clicks > 4) {
-                $(".description").text("klikov");
-            }
-        })
-    })
+    new Vue({
+        el: '.container',
+        data: {
+            counter: 0
+        }
+    });
+
 </script>
 
 </html>
--- vybersi.php
+++ vybersi.php
@@ -1,69 +1,68 @@
 <!DOCTYPE html>
 <!--suppress ALL -->
 <html>
-<?php include 'partials/head.php'; ?>
+<script src="https://cdn.jsdelivr.net/npm/vue"></script>
 <link href="https://fonts.googleapis.com/css?family=Roboto:500i" rel="stylesheet">
+<?php include 'partials/head.php'; ?>
 <style>
-    body {
-        background: url("img/pokemon/background.jpg") no-repeat center center fixed;
-        -webkit-background-size: cover;
-        -moz-background-size: cover;
-        -o-background-size: cover;
-        background-size: cover;
-    }
-    h1{
-        margin-bottom: 30px;
-    }
+  body {
+    background: url("img/pokemon/background.jpg") no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+  }
 
-    h1,h3 {
-        font-family: Roboto, sans-serif;
-        font-size: 70px;
-        color:#FFFFFF;
-        text-shadow: 3px 3px 2px #68696b;
-    }
+  h1 {
+    margin-bottom: 30px;
+  }
 
+  h1, h3 {
+    font-family: Roboto, sans-serif;
+    font-size: 70px;
+    color: #FFFFFF;
+    text-shadow: 3px 3px 2px #68696b;
+  }
 
-    div.pokemon {
-        margin-top: 50px;
-    }
+  div.pokemon {
+    margin-top: 50px;
+  }
 
-    select {
-        margin-top: 30px;
-    }
+  select {
+    margin-top: 30px;
+  }
 
 </style>
 <body>
 <?php include 'partials/navigation.php' ?>
 <div class="container">
-    <h1 class="text-center">Select 'em all!</h1>
-    <div class="col-md-6 col-md-offset-3">
-        <select class="form-control">
-            <option value="">select your pokemon</option>
-            <option value="01">Charmander</option>
-            <option value="02">Bulbasaur</option>
-            <option value="03">Squirtle</option>
-            <option value="04">Pikachu</option>
-            <option value="05">Diglett</option>
-            <option value="06">Geodude</option>
-        </select>
-    </div>
-    <div class="col-md-10 col-md-offset-1 pokemon text-center">
-    </div>
+  <h1 class="text-center">Select 'em all!</h1>
+  <div class="col-md-6 col-md-offset-3">
+    <select class="form-control" v-model="selected">
+      <option disabled value="">Please select one</option>
+      <option v-for="option in options" v-bind:value="option">{{option.text}}</option>
+    </select>
+  </div>
+  <div v-if="selected" class="col-md-10 col-md-offset-1 pokemon text-center">
+    <img v-bind:src="'img/pokemon/'+selected.value+'.png'" alt="">
+    <h3>I choose you {{selected.text}} !</h3>
+  </div>
 </div>
 </body>
 <script>
-    var selectedPokemon;
-    $(document).ready(function () {
-        $("select").change(function () {
-            selectedPokemon = $(this).find(":selected");
-            if (selectedPokemon.val()) {
-                $("div.pokemon")
-                    .html("<img src=\"img/pokemon/" + selectedPokemon.val() + ".png\" alt=\"\">")
-                    .append("<h3>I choose you " + selectedPokemon.text() + " !</h3>");
-            }
-        });
-
+    new Vue({
+        el: '.container',
+        data: {
+            selected: '',
+            options: [
+                {value: '01', text: 'Charmander'},
+                {value: '02', text: 'Bulbasaur'},
+                {value: '03', text: 'Squirtle'},
+                {value: '04', text: 'Pikachu'},
+                {value: '05', text: 'Diglett'},
+                {value: '06', text: 'Geodude'}
+            ]
+        }
     })
-
 </script>
 </html>
